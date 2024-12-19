f893e6743de46618f5f92f83fc2865c4ff0b873c
Roman Stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2024-12-19 22:54:44 +0000

    demonstrate rewrite url

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,23 +2,10 @@ import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 
 const server = {
-  signUp: defineAction({
+  testAction: defineAction({
     accept: "form",
-    input: z
-      .object({
-        name: z.string().trim().min(1).max(200),
-        email: z.string().trim().min(1).email().max(200),
-        password: z.string().min(8).max(200),
-        repeatPassword: z.string().min(8).max(200),
-      })
-      .refine(({ password, repeatPassword }) => password === repeatPassword, {
-        message: "Passwords don't match",
-        path: ["repeatPassword"],
-      }),
-    handler: async (input) => {
-      console.log(input);
-      return 42;
-    },
+    input: z.object({}),
+    handler: async () => 42,
   }),
 };
 
